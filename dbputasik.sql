ebc369661b47611098b1b63ab7b415f05664fdb9
MGX82 <rifky@kudo.co.id>
2017-09-24 21:34:20 +0700

    database relation location

--- dbputasik.sql
+++ dbputasik.sql
@@ -1,56 +1,67 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 24, 2017 at 10:58 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Source Server         : LocalAppache
+ Source Server Type    : MySQL
+ Source Server Version : 50505
+ Source Host           : localhost
+ Source Database       : dbputasik
 
+ Target Server Type    : MySQL
+ Target Server Version : 50505
+ File Encoding         : utf-8
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 09/24/2017 21:28:40 PM
+*/
 
---
--- Database: `dbputasik`
---
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `iduser` int(11) NOT NULL,
-  `user` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+-- ----------------------------
+--  Table structure for `t_city`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_city`;
+CREATE TABLE `t_city` (
+  `id_kota` int(11) NOT NULL AUTO_INCREMENT,
+  `Name_city` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_kota`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+-- ----------------------------
+--  Table structure for `t_districts`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_districts`;
+CREATE TABLE `t_districts` (
+  `id_districts` int(11) NOT NULL AUTO_INCREMENT,
+  `name_districts` varchar(50) DEFAULT NULL,
+  `id_kota` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_districts`),
+  KEY `id_kota` (`id_kota`),
+  CONSTRAINT `id_kota` FOREIGN KEY (`id_kota`) REFERENCES `t_city` (`id_kota`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`iduser`);
+-- ----------------------------
+--  Table structure for `t_village`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_village`;
+CREATE TABLE `t_village` (
+  `id_village` int(11) NOT NULL AUTO_INCREMENT,
+  `name_village` varchar(50) DEFAULT NULL,
+  `id_districts` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id_village`),
+  KEY `id_districts` (`id_districts`),
+  CONSTRAINT `id_districts` FOREIGN KEY (`id_districts`) REFERENCES `t_districts` (`id_districts`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for dumped tables
---
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
